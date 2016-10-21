25284bd51d2739d6dc4ac5ca6981bfdc7900213a
Leo Bossmann <leo@bossmann.biz>
2016-10-21 17:38:14 +0200

    Fixed mail notifications

--- lib/shoplifty/mail_notifications.rb
+++ lib/shoplifty/mail_notifications.rb
@@ -1,14 +1,27 @@
 module Shoplifty
   class MailNotifications
-    def initialize()
-      @notifications = {}
-      raw_notifications = Shoplifty::client.get('/admin/email_templates.json')
-      raw_notifications['email_templates'].each do |n|
-        @notifications[n['name'].underscore] = ShopifyEntity.new('email_templates', n)
-      end
+    def initialize(template)
+      @email_form = Shoplifty::client.agent.get("/admin/email_templates/#{template}/edit").form_with(action: /email_templates/)
     end
-    def method_missing(m)
-      @notifications[m.to_s]
+
+    def title=(value)
+      email_field('title', value)
+    end
+
+    def body_html=(value)
+      email_field('body_html', value)
+    end
+
+    def body=(value)
+      email_field('body', value)
+    end
+
+    def email_field(field, value)
+      @email_form.field_with(name: "email_template[#{field}]") {|field| field.value = value}
+    end
+
+    def save
+      @email_form.submit
     end
   end
 end
